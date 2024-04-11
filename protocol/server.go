495140b7bbbf19cfca76d7ddbf1768be9b75155d
xiaodong <373232355@qq.com>
2024-04-12 00:30:59 +0800

    temporary storage

--- protocol/server.go
+++ protocol/server.go
@@ -3,5 +3,11 @@ package protocol
 import "github.com/panjf2000/gnet"
 
 type echoServer struct {
-	gnet.EventServer
+	*gnet.EventServer
+}
+
+func (es *echoServer) Ping(c gnet.Conn) (out []byte, action gnet.Action) {
+	out = c.Read()
+	c.ResetBuffer()
+	return
 }
