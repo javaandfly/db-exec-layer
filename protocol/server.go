0972ded2c62265d56e1fa46581ac23ec8c9fef8b
董志 <dongzhi@hqshuke.com>
2024-04-12 17:38:53 +0800

    add protofile

--- protocol/server.go
+++ protocol/server.go
@@ -3,5 +3,10 @@ package protocol
 import "github.com/panjf2000/gnet"
 
 type echoServer struct {
-	gnet.EventServer
+	*gnet.EventServer
+}
+
+func (es *echoServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
+	out = frame
+	return
 }
