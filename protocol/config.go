aefa3f245710f39953b361963a1710381799e8af
董志 <dongzhi@hqshuke.com>
2024-04-17 17:36:52 +0800

    add heart beat

--- protocol/config.go
+++ protocol/config.go
@@ -14,9 +14,11 @@ const (
 	socketRingBufferSize = 1024
 
 	//协议行为定义
-	ACTION_PING = 0x0001 // ping行为
-	ACTION_PONG = 0x0002 // pong行为
-	ACTION_DATA = 0x00F0 // 业务行为
+	ACTION_PING            = 0x0001 // ping行为
+	ACTION_PONG            = 0x0002 // pong行为
+	ACTION_HEART_BEAT      = 0x0003 // 心跳行为
+	ACTION_HEART_BEAT_RESP = 0x0004 //心跳返回行为
+	ACTION_DATA            = 0x00F0 // 业务行为
 
 )
 
